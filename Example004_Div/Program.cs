1809799f8fec57c719f72f3b084718d145034f68
AymanMuur <82535317+AymanMuur@users.noreply.github.com>
2023-11-20 19:07:13 +0300

    added Example004_Div1

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 12;
-double numderB = 5;
+﻿double numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+double numderB = new Random().Next(1, 10);
+Console.WriteLine(numderB);
 Console.WriteLine(numberA/numderB);
 
